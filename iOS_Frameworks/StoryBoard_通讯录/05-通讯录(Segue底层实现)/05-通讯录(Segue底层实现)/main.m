69079c88ae020ae785b39b3c4af88480e1e10520
吴佳臻 <elzza583431183@gmail.com>
2018-04-28 17:03:59 +0800

    segue的原理

---
+++ iOS_Frameworks/StoryBoard_通讯录/05-通讯录(Segue底层实现)/05-通讯录(Segue底层实现)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01-通讯录(界面搭建)
+//
+//  Created by 吴佳臻 on 2018/4/28.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
