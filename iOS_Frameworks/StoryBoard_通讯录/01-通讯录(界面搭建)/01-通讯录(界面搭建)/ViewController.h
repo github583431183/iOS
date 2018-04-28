be653f84d49db459e3f875023bc1c1616f7bf47b
吴佳臻 <elzza583431183@gmail.com>
2018-04-28 10:18:38 +0800

    storyboard_通讯录

---
+++ iOS_Frameworks/StoryBoard_通讯录/01-通讯录(界面搭建)/01-通讯录(界面搭建)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-通讯录(界面搭建)
+//
+//  Created by 吴佳臻 on 2018/4/28.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_Frameworks/StoryBoard_通讯录/01-通讯录(界面搭建)/01-通讯录(界面搭建)/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  01-通讯录(界面搭建)
+//
+//  Created by 吴佳臻 on 2018/4/28.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
