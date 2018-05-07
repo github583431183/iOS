aaa02b4f2c5074141feb738b48c261376713b432
wjz <elzza583431183@gmail.com>
2018-05-07 11:23:46 +0800

    007_08_UITableViewController

---
+++ iOS_Basic/007_08_UITableViewController/007_08_UITableViewController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  007_08_UITableViewController
+//
+//  Created by 吴佳臻 on 2018/5/7.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ iOS_Basic/007_08_UITableViewController/007_08_UITableViewController/WJZTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WJZTableViewController.h
+//  007_08_UITableViewController
+//
+//  Created by 吴佳臻 on 2018/5/7.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJZTableViewController : UITableViewController
+
+@end
