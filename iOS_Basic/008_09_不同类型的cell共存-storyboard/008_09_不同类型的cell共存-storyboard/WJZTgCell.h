ac1df02245ea41b2ddd3c847b514bb4ae1975a34
wjz <elzza583431183@gmail.com>
2018-05-09 14:18:40 +0800

    008_09_不同类型的cell共存-storyboard

---
+++ iOS_Basic/008_09_不同类型的cell共存-storyboard/008_09_不同类型的cell共存-storyboard/WJZTgCell.h
@@ -0,0 +1,14 @@
+//
+//  WJZTgCell.h
+//  008_08_自定义等高的cell-storyboard
+//
+//  Created by 吴佳臻 on 2018/5/9.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class WJZTg;
+@interface WJZTgCell : UITableViewCell
+
+@property(nonatomic,strong) WJZTg *tg;
+@end
