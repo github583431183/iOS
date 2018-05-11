051d940c727a1eb7bb43624774510de82f4df89b
wjz <elzza583431183@gmail.com>
2018-05-11 15:12:24 +0800

    009_06-左滑删除功能1

---
+++ iOS_Basic/009_06-左滑删除功能1/009_06-左滑删除功能1/ViewController.m
@@ -0,0 +1,85 @@
+//
+//  ViewController.m
+//  009_04_数据刷新01-全局刷新
+//
+//  Created by 吴佳臻 on 2018/5/11.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "WJZWine.h"
+#import "WJZWineCell.h"
+#import "MJExtension.h"
+@interface ViewController ()
+@property(nonatomic,strong)NSMutableArray *wineArray;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
+
+@implementation ViewController
+
+-(NSMutableArray *)wineArray{
+    if (!_wineArray) {
+        _wineArray = [WJZWine mj_objectArrayWithFilename:@"wine.plist"];
+    }
+    return _wineArray;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 按钮的点击
+
+- (IBAction)remove:(id)sender {
+    
+    
+}
+
+#pragma mark - UITableViewDelegate
+/**
+ *  只要实现这个方法,就拥有左滑删除功能
+ *  点击左滑出现的Delete按钮 会调用这个
+ */
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self.wineArray removeObjectAtIndex:indexPath.row];
+    [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+}
+
+/**
+ *  修改默认Delete按钮的文字
+ */
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return @"删除";
+}
+
+
+#pragma mark - UITableViewDataSource
+//@required
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.wineArray.count;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *ID = @"wine";
+    WJZWineCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[WJZWineCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    cell.wine = self.wineArray[indexPath.row];
+    return cell;
+}
+
+
+
+@end
---
+++ iOS_Basic/009_06-左滑删除功能1/009_06-左滑删除功能1/WJZWine.h
@@ -0,0 +1,15 @@
+//
+//  WJZWine.h
+//  009_04_数据刷新01-全局刷新
+//
+//  Created by 吴佳臻 on 2018/5/11.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WJZWine : NSObject
+@property(nonatomic,copy)NSString *image;
+@property(nonatomic,copy)NSString *money;
+@property(nonatomic,copy)NSString *name;
+@end
