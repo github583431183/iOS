daa799157d0724bcfe817c50bcca39bff5467950
wjz <elzza583431183@gmail.com>
2018-05-15 13:59:45 +0800

    05_彩票_自定义tabBar_3

---
+++ iOS_Frameworks/003_彩票_纯代码/05_彩票_自定义tabBar_3/05_彩票_自定义tabBar_3/Classes/Main(主要)/View/WJZTabBar.h
@@ -0,0 +1,18 @@
+//
+//  WJZTabBar.h
+//  05_彩票_自定义tabBar_3
+//
+//  Created by 吴佳臻 on 2018/5/15.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJZTabBar : UIView
+/** 子控制器的个数 */
+//@property (nonatomic, assign) NSInteger count;
+
+
+/** 模型数组 */
+@property (nonatomic, strong) NSArray *items;
+@end
