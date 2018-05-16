76bfcb62d32ea88345508edf1150c6d7dc519104
wjz <elzza583431183@gmail.com>
2018-05-16 13:08:08 +0800

    09-购彩大厅

---
+++ iOS_Frameworks/003_彩票_纯代码/09-购彩大厅/09-购彩大厅/Classes/Hall(购彩大厅)/Controller/WJZHallTableViewController.m
@@ -0,0 +1,116 @@
+//
+//  WJZHallTableViewController.m
+//  03_彩票_自定义tabBar_1
+//
+//  Created by 吴佳臻 on 2018/5/14.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import "WJZHallTableViewController.h"
+#import "UIImage+WJZ.h"
+@interface WJZHallTableViewController ()
+
+@end
+
+@implementation WJZHallTableViewController
+
+-(void)viewDidAppear:(BOOL)animated{
+    
+    NSLog(@"viewDidAppear--->%@",NSStringFromCGRect(self.view.frame));
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+     NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    //在iOS 7之后导航条上的图片，默认被渲染,渲染成蓝色
+    //1.设置导航条左侧按钮
+    //禁止图片渲染
+//    UIImage *image = [UIImage imageNamed:@"CS50_activity_image"];
+//    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+    //使用分类设置不渲染的图片
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithRenderOriginalName:@"CS50_activity_image"] style:UIBarButtonItemStylePlain target:self action:@selector(leftButtonOnClick)];
+}
+
+-(void)leftButtonOnClick{
+    NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
