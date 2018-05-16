e8c2c1033c5b9c9664e8dcfa2526fe6de56f7d29
wjz <elzza583431183@gmail.com>
2018-05-16 15:44:27 +0800

    12-自定义pop菜单下

---
+++ iOS_Frameworks/003_彩票_纯代码/12-自定义pop菜单下/12-自定义pop菜单下/Classes/Hall(购彩大厅)/View/WJZPopMenu.h
@@ -0,0 +1,26 @@
+//
+//  WJZPopMenu.h
+//  11-自定义pop菜单上
+//
+//  Created by 吴佳臻 on 2018/5/16.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WJZPopMenu;
+
+@protocol WJZPopMenuDelegate <NSObject>
+
+- (void)popMenuDidCloseBtn:(WJZPopMenu *)popMenu;
+- (void)popMenuAnimCompletion:(WJZPopMenu *)popMenu;
+@end
+
+@interface WJZPopMenu : UIView
+
++ (instancetype)showInCenter:(CGPoint)center;
+
+- (void)hideInCenter:(CGPoint)center;
+
+@property (nonatomic, weak) id<WJZPopMenuDelegate> delegate;
+@end
