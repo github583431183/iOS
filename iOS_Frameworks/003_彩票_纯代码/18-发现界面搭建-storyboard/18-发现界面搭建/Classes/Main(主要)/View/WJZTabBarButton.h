6e7d5f8c5504bc2cf5737f1b65cde99362d7dd92
wjz <elzza583431183@gmail.com>
2018-05-18 15:01:13 +0800

    18-发现界面搭建-storyboard

---
+++ iOS_Frameworks/003_彩票_纯代码/18-发现界面搭建-storyboard/18-发现界面搭建/Classes/Main(主要)/View/WJZTabBarButton.h
@@ -0,0 +1,13 @@
+//
+//  WJZTabBarButton.h
+//  05_彩票_自定义tabBar_3
+//
+//  Created by 吴佳臻 on 2018/5/15.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJZTabBarButton : UIButton
+
+@end
