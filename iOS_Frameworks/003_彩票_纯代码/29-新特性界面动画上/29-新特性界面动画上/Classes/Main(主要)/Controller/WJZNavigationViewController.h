c0f6d0d4b4a55455ade8cf3778a0690cb9fa24d9
wjz <elzza583431183@gmail.com>
2018-05-22 13:36:11 +0800

    29-新特性界面动画上

---
+++ iOS_Frameworks/003_彩票_纯代码/29-新特性界面动画上/29-新特性界面动画上/Classes/Main(主要)/Controller/WJZNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  WJZNavigationViewController.h
+//  08-自定义导航控制器
+//
+//  Created by 吴佳臻 on 2018/5/16.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJZNavigationViewController : UINavigationController
+
+@end
