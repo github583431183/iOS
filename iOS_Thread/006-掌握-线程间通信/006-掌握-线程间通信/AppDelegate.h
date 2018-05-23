d79908c8e0d70d12c36f019aa8cdefea6057849f
wjz <elzza583431183@gmail.com>
2018-05-23 17:26:44 +0800

    006-掌握-线程间通信

---
+++ iOS_Thread/006-掌握-线程间通信/006-掌握-线程间通信/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  006-掌握-线程间通信
+//
+//  Created by 吴佳臻 on 2018/5/23.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Thread/006-掌握-线程间通信/006-掌握-线程间通信/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  006-掌握-线程间通信
+//
+//  Created by 吴佳臻 on 2018/5/23.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
