0ae5334a71c2362db485b2a980cefa368c9796d2
wjz <elzza583431183@gmail.com>
2018-06-11 16:21:12 +0800

    301-掌握-GCD中的定时器

---
+++ iOS_Thread/301-掌握-GCD中的定时器/301-掌握-GCD中的定时器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  301-掌握-GCD中的定时器
+//
+//  Created by 吴佳臻 on 2018/6/11.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Thread/301-掌握-GCD中的定时器/301-掌握-GCD中的定时器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  301-掌握-GCD中的定时器
+//
+//  Created by 吴佳臻 on 2018/6/11.
+//  Copyright © 2018年 wjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
